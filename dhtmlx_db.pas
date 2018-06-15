8715763044650dd71858f44d3948bd8d3f82a44e
Chris <ulrich@tcs-germany.de>
2018-06-15 16:30:06 +0200

    Add intial Data

--- dhtmlx_db.pas
+++ dhtmlx_db.pas
@@ -21,6 +21,8 @@ type
     procedure UpdateData; override;
     procedure RecordChanged(Field: TField); override;
     procedure ActiveChanged; override;
+    function GetRecordCount: Integer; override;
+    procedure DataEvent(Event: TDataEvent; Info: JSValue); override;
   public
     constructor Create;
     property Dataprocessor : TDHTMLXDataProcessor read FDataprocessor;
@@ -49,16 +51,53 @@ end;
 
 procedure TDHTMLXDataLink.ActiveChanged;
   procedure AddRows;
+  var
+    i, a: Integer;
+    aObj: TJSObject;
   begin
-    Datastore.add(js.new([]));
-    Datastore.add(js.new([]));
-    Datastore.add(js.new([]));
+    DataSet.DisableControls;
+    for i := 0 to RecordCount-1 do
+      begin
+        aObj := TJSObject.new;
+        ActiveRecord:=i;
+        for a := 0 to DataSet.FieldCount-1 do
+          aObj.Properties[DataSet.Fields[a].FieldName] := DataSet.Fields[a].AsString;
+        Datastore.add(aObj);
+      end;
+    DataSet.EnableControls;
   end;
 
 begin
   writeln('ActiveChanged');
   inherited ActiveChanged;
   Dataprocessor.ignore(@AddRows);
+end;
+
+function TDHTMLXDataLink.GetRecordCount: Integer;
+begin
+  Result:=DataSet.RecordCount;
+end;
+
+procedure TDHTMLXDataLink.DataEvent(Event: TDataEvent; Info: JSValue);
+begin
+  case Event of
+  deFieldChange:writeln('DataEvent ','deFieldChange');
+  deRecordChange:writeln('DataEvent ','deRecordChange');
+  deDataSetChange:writeln('DataEvent ','deDataSetChange');
+  deDataSetScroll:writeln('DataEvent ','deDataSetScroll');
+  deLayoutChange:writeln('DataEvent ','deLayoutChange');
+  deUpdateRecord:writeln('DataEvent ','deUpdateRecord');
+  deUpdateState:writeln('DataEvent ','deUpdateState');
+  deCheckBrowseMode:writeln('DataEvent ','deCheckBrowseMode');
+  dePropertyChange:writeln('DataEvent ','dePropertyChange');
+  deFieldListChange:writeln('DataEvent ','deFieldListChange');
+  deFocusControl:writeln('DataEvent ','deFocusControl');
+  deParentScroll:writeln('DataEvent ','deParentScroll');
+  deConnectChange:writeln('DataEvent ','deConnectChange');
+  deReconcileError:writeln('DataEvent ','deReconcileError');
+  deDisabledStateChange:writeln('DataEvent ','deDisabledStateChange');
+  end;
+  inherited;
 end;
 
 end.
