784db3f91f9eba13b6c33f0393618d67980a5491
Chris <ulrich@tcs-germany.de>
2018-06-21 08:37:56 +0200

    allow to use AppendCSS and JS outside of dhtmlx

--- dhtmlx_base.pas
+++ dhtmlx_base.pas
@@ -31,45 +31,53 @@ type
     isOpera : Boolean;
     function version : string;
   end;
+  Tdhx4 = class external name 'dhx4' (TJSElement)
+    skin : string;
+  end;
+
+procedure AppendCSS(url : string;onLoad,onError : JSValue);
+procedure AppendJS(url : string;onLoad,onError : JSValue);
 
 var
   dhtmlx : Tdhtmlx external name 'dhtmlx';
   dhx : Tdhx external name 'window.dhx';
+  dhx4 : Tdhx external name 'window.dhx4';
   WidgetsetLoaded : TJSPromise;
 
 
 implementation
 
-procedure LoadDHTMLX;
-  procedure AppendCSS(url : string;onLoad,onError : JSValue);
-  begin
-    asm
+procedure AppendCSS(url : string;onLoad,onError : JSValue);
+begin
+  asm
+    var file = url;
+    var link = document.createElement( "link" );
+    link.href = file;
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.media = "screen,print";
+    link.onload = onLoad;
+    link.onerror = onError;
+    document.getElementsByTagName( "head" )[0].appendChild( link );
+  end;
+end;
+procedure AppendJS(url : string;onLoad,onError : JSValue);
+begin
+  asm
+    if (document.getElementById(url) == null) {
       var file = url;
-      var link = document.createElement( "link" );
-      link.href = file;
-      link.type = "text/css";
-      link.rel = "stylesheet";
-      link.media = "screen,print";
+      var link = document.createElement( "script" );
+      link.id = url;
+      link.src = file;
+      link.type = "text/javascript";
       link.onload = onLoad;
       link.onerror = onError;
       document.getElementsByTagName( "head" )[0].appendChild( link );
-    end;
+    }
   end;
-  procedure AppendJS(url : string;onLoad,onError : JSValue);
-  begin
-    asm
-      if (document.getElementById(url) == null) {
-        var file = url;
-        var link = document.createElement( "script" );
-        link.id = url;
-        link.src = file;
-        link.type = "text/javascript";
-        link.onload = onLoad;
-        link.onerror = onError;
-        document.getElementsByTagName( "head" )[0].appendChild( link );
-      }
-    end;
-  end;
+end;
+
+procedure LoadDHTMLX;
   procedure DoLoadDHTMLX(resolve,reject : TJSPromiseResolver) ;
     procedure ScriptLoaded;
     begin
